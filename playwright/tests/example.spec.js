4e6e7de7816eadcd91a1e507745772c18d08018f
Oleksii30 <polyanovskyyol@gmail.com>
2023-06-27 18:20:09 +0300

    try local server

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -1,11 +1,11 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-// test('has laravel text', async ({ page }) => {
-//   await page.goto('http://127.0.0.1:8000');
+test('has laravel text local server', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000');
 
-//   await expect(page.getByText('You have arrived')).toBeVisible();
-// });
+  await expect(page.getByText('You have arrived')).toBeVisible();
+});
 
 
 test('has laravel text', async ({ page }) => {
