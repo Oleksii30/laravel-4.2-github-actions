f7c98f5ff056c023815344afb83f8dcc6833e53f
Oleksii30 <polyanovskyyol@gmail.com>
2023-06-27 17:10:57 +0300

    check if test is running

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -2,8 +2,11 @@
 const { test, expect } = require('@playwright/test');
 
 test('has laravel text', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8000');
+  console.log('try if console log is working here')
+  await page.goto('https://laravel.com/');
+  //await page.goto('http://127.0.0.1:8000');
 
-  await expect(page.getByText('You have arrived')).toBeVisible();
+  //await expect(page.getByText('You have arrived')).toBeVisible();
+  await expect(page.getByText('The PHP Framework')).toBeVisible();
 });
 
