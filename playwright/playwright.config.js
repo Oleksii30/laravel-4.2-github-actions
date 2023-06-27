3221ae4e77464df8ebc4d6b3d8aa97932113b8a1
Oleksii30 <polyanovskyyol@gmail.com>
2023-06-27 18:35:26 +0300

    try change server command

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -73,7 +73,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'cd playwright && npm run start',
     url: 'http://127.0.0.1:8000',
     //reuseExistingServer: !process.env.CI,
   },
