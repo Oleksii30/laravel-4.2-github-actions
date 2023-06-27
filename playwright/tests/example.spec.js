a0fedae9f4ad08dbdd25f30211d2f32ba18fc7e0
Oleksii30 <polyanovskyyol@gmail.com>
2023-06-27 17:30:20 +0300

    check with report-1

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -2,11 +2,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('has laravel text', async ({ page }) => {
-  console.log('try if console log is working here')
-  await page.goto('https://laravel.com/');
-  //await page.goto('http://127.0.0.1:8000');
+  await page.goto('http://127.0.0.1:8000');
 
-  //await expect(page.getByText('You have arrived')).toBeVisible();
-  await expect(page.getByText('The PHP Framework')).toBeVisible();
+  await expect(page.getByText('You have arrived')).toBeVisible();
 });
 
